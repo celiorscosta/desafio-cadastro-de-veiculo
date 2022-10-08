4ae418da910c1ede3db0258e6b567c0301d7a173
celiorscosta <celiorscosta@hotmail.com>
2022-10-08 09:35:46 -0300

    Swagger para get all

--- backend-api/src/app.js
+++ backend-api/src/app.js
@@ -6,9 +6,9 @@ const swaggerDocument = require ('./swagger.json');
 const app = express();
 const port = 3000;
 
-routes(app);
-
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
+
+routes(app);
 
 app.listen(port, ()=> console.log(`Servidor esta rodando na porta ${port}`));
 
